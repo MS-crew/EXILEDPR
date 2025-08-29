ef0195c9ea14df8f77379b4fabb4418586b0eb9c
MS-crew <100300664+MS-crew@users.noreply.github.com>
2025-08-29 17:10:59 +0300

    Update ReloadingWeapon.cs

--- EXILED/Exiled.Events/Patches/Events/Player/ReloadingWeapon.cs
+++ EXILED/Exiled.Events/Patches/Events/Player/ReloadingWeapon.cs
@@ -12,7 +12,6 @@ namespace Exiled.Events.Patches.Events.Player
 
     using API.Features.Pools;
 
-    using Exiled.API.Extensions;
     using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Player;
     using HarmonyLib;
@@ -33,10 +32,11 @@ namespace Exiled.Events.Patches.Events.Player
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
+            LocalBuilder ev1 = generator.DeclareLocal(typeof(ReloadingWeaponEventArgs));
+            LocalBuilder ev2 = generator.DeclareLocal(typeof(UnloadingWeaponEventArgs));
+
             int offset = -2;
             int index = newInstructions.FindIndex(x => x.Calls(Method(typeof(IReloadUnloadValidatorModule), nameof(IReloadUnloadValidatorModule.ValidateReload)))) + offset;
-
-            Label stopMessage = generator.DefineLabel();
 
             newInstructions.InsertRange(
                 index,
@@ -49,17 +49,23 @@ namespace Exiled.Events.Patches.Events.Player
                     // ReloadingWeaponEventArgs ev = new(firearm)
                     new(OpCodes.Newobj, GetDeclaredConstructors(typeof(ReloadingWeaponEventArgs))[0]),
                     new(OpCodes.Dup),
+                    new(OpCodes.Stloc_S, ev1.LocalIndex),
 
                     // Player.OnReloadingWeapon(ev)
                     new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnReloadingWeapon))),
+                });
 
-                    // if (!ev.IsAllowed)
-                    //    this.SendRpc(NetworkWriter x =>
-                    //    {
-                    //      x.WriteSubheader(AnimatorReloaderModuleBase.ReloaderMessageHeader.Stop);
-                    //    }, true);
+            offset = 1;
+            index = newInstructions.FindIndex(x => x.Calls(Method(typeof(IReloadUnloadValidatorModule), nameof(IReloadUnloadValidatorModule.ValidateReload)))) + offset;
+
+            newInstructions.InsertRange(
+                index,
+                new CodeInstruction[]
+                {
+                    // flag = IReloadUnloadValidatorModule.ValidateReload(base.Firearm) && ev.IsAllowed;
+                    new(OpCodes.Ldloc_S, ev1.LocalIndex),
                     new(OpCodes.Callvirt, PropertyGetter(typeof(ReloadingWeaponEventArgs), nameof(ReloadingWeaponEventArgs.IsAllowed))),
-                    new(OpCodes.Brfalse, stopMessage),
+                    new(OpCodes.And),
                 });
 
             offset = -2;
@@ -76,23 +82,24 @@ namespace Exiled.Events.Patches.Events.Player
                     // UnloadingWeaponEventArgs ev = new(firearm)
                     new(OpCodes.Newobj, GetDeclaredConstructors(typeof(UnloadingWeaponEventArgs))[0]),
                     new(OpCodes.Dup),
+                    new(OpCodes.Stloc_S, ev2.LocalIndex),
 
                     // Player.OnUnloadingWeapon(ev)
                     new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnUnloadingWeapon))),
-
-                    // if (!ev.IsAllowed)
-                    //    this.SendRpc(NetworkWriter x =>
-                    //    {
-                    //      x.WriteSubheader(AnimatorReloaderModuleBase.ReloaderMessageHeader.Stop);
-                    //    }, true);
-                    new (OpCodes.Callvirt, PropertyGetter(typeof(UnloadingWeaponEventArgs), nameof(UnloadingWeaponEventArgs.IsAllowed))),
-                    new(OpCodes.Brfalse, stopMessage),
                 });
 
-            offset = 2;
-            index = newInstructions.FindIndex(x => x.operand == (object)PropertyGetter(typeof(AnimatorReloaderModuleBase), nameof(AnimatorReloaderModuleBase.IsUnloading))) + offset;
+            offset = 1;
+            index = newInstructions.FindIndex(x => x.Calls(Method(typeof(IReloadUnloadValidatorModule), nameof(IReloadUnloadValidatorModule.ValidateUnload)))) + offset;
 
-            newInstructions[index].labels.Add(stopMessage);
+            newInstructions.InsertRange(
+                index,
+                new CodeInstruction[]
+                {
+                    // flag = IReloadUnloadValidatorModule.ValidateUnload(base.Firearm) && ev.IsAllowed;
+                    new(OpCodes.Ldloc_S, ev2.LocalIndex),
+                    new (OpCodes.Callvirt, PropertyGetter(typeof(UnloadingWeaponEventArgs), nameof(UnloadingWeaponEventArgs.IsAllowed))),
+                    new(OpCodes.And),
+                });
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
