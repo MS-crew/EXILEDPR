ec3e1520ed5c6bd640134a4bbfbf9dcad4e9fc8c
MS-crew <100300664+MS-crew@users.noreply.github.com>
2025-08-29 22:33:22 +0300

    Create FixCapturePosition.cs

---
+++ EXILED/Exiled.Events/Patches/Fixes/FixCapturePosition.cs
@@ -0,0 +1,36 @@
+// -----------------------------------------------------------------------
+// <copyright file="FixCapturePosition.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Fixes
+{
+    using CustomPlayerEffects;
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using HarmonyLib;
+    using RelativePositioning;
+    using UnityEngine;
+
+    /// <summary>
+    /// Patches <see cref="PocketCorroding.CapturePosition" />'s setter.
+    /// Fix for those who go to pocket without effect and to get empty or null capture position and fall into the void.
+    /// </summary>
+    [HarmonyPatch(typeof(PocketCorroding), nameof(PocketCorroding.CapturePosition), MethodType.Getter)]
+    internal class FixCapturePosition
+    {
+        private const RoomType DefaultRoomType = RoomType.Surface;
+
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+        private static void Postfix(PocketCorroding __instance, ref RelativePosition __result)
+        {
+            if (__result.Position != Vector3.zero)
+                return;
+
+            Room room = Room.Get(DefaultRoomType);
+            __result = new RelativePosition(room.Position);
+        }
+    }
+}
